b1770e306a0d6024f82d7fe2c764c71545bc0acd
Climent Fernández <a23cliferand@inspedralbes.cat>
2025-01-27 23:23:53 +0100

    Decadence 4

--- app/src/main/java/com/example/tr2_process/MainActivity.kt
+++ app/src/main/java/com/example/tr2_process/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
                 val viewModel = remember { ServiceViewModel(application) }
-                val hostConfig1 = HostConfigEntity(name = "Community&Employument", host = "http://conexus.dam.inspedralbes.cat", port = "24848", enable = true)
-                val hostConfig2 = HostConfigEntity(name = "Chat,IaIamge,IaText,Notifications,Statisics", host = "http://conexus.dam.inspedralbes.cat", port = "24852", enable = false)
+                val hostConfig1 = HostConfigEntity(name = "Local", host = "http://10.0.2.2", port = "24848", enable = true)
+                val hostConfig2 = HostConfigEntity(name = "Prod", host = "http://conexus.dam.inspedralbes.cat", port = "24848", enable = false)
                 viewModel.insertHostConfig(hostConfig1)
                 viewModel.insertHostConfig(hostConfig2)
 
